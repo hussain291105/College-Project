0e0f80e46b1bdd11db8791dcd47ec3165c153ce7
Hussain <hussain.f.rang29@gmail.com>
2025-11-25 22:23:43 +0530

    new changes 02

--- server/db.js
+++ server/db.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306,
 });
 
 db.connect((err) => {
